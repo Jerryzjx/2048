9ea2a71d4b6be7c71e1c10364da84fc414581017
JerryZhang0920 <jinxuan0920@gmail.com>
2021-02-12 03:58:01 +0800

    Finished function to read and check if user name exists, if not add a user name

--- src/com/company/_2048.java
+++ src/com/company/_2048.java
@@ -12,14 +12,22 @@ public class _2048 {
     public static void main(String[] args) throws IOException {
         User_Access();
         String filename = "C:\\Users\\Leonard\\IdeaProjects\\2048\\src\\com\\company\\users.txt";
-        Read_user(filename);
-
+        ArrayList<String> users = Read_user(filename);
+        for (int i = 0; i < users.size(); i++) {
+            if (user_name.equals(users.get(i))) {
+                System.out.println("Username exists");
+            } else {
+                System.out.println("New User Added");
+                new_user(filename);
+            }
+            break;
+        }
     }
     public static void User_Access() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to 2048 game, please type in your username below");
         boolean flag_user = false;
-        while (flag_user == false) {
+        while (!flag_user) {
             try {
                 user_name = sc.next();
                 flag_user = true;
@@ -30,19 +38,36 @@ public class _2048 {
             }
         }
     }
-    public static void Read_user (String filename) throws IOException {
+    public static ArrayList <String> Read_user (String filename) throws IOException {
 
         FileReader fr = new FileReader("C:\\Users\\Leonard\\IdeaProjects\\2048\\src\\com\\company\\users.txt");
         BufferedReader in = new BufferedReader(fr);
         String line = in.readLine();
         int i = 0;
-        String[] users = new String[20];
-        while(line != null){
-            users[i] = String.valueOf(line.split(" "));
-            if(users[i].equals(user_name)){
-                System.out.println("Username exists!");
-                break;
-            }
+       ArrayList<String> userList;
+       userList = new ArrayList<>();
+        while(line != null && i<i+1 && i<25){
+            userList.add(in.readLine());
+           //System.out.println(userList.get(i));
+           i++;
         }
+
+        return userList;
+    }
+    public static void new_user (String filename) throws IOException {
+
+        BufferedWriter output = new BufferedWriter(new FileWriter("C:\\Users\\Leonard\\IdeaProjects\\2048\\src\\com\\company\\users.txt", true));
+        output.append("\n").append(user_name);
+       output.close();
+    }
+
+    public static void check_user_file_exist() {
+
+    }
+    public static void write_highest_score(){
+
+    }
+    public static void save_highest_score(){
+
     }
 }
