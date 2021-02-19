202302794def3797a87e845126191f69c8023ebb
JerryZhang0920 <jinxuan0920@gmail.com>
2021-02-20 03:02:11 +0800

    added function to read from saved grid

--- src/com/company/_2048.java
+++ src/com/company/_2048.java
@@ -8,9 +8,14 @@ import java.util.*;
 // Project name: 2048 with file io
 // purpose: make an unbreakable 2048 game!
 public class _2048 {
+    public static final String ANSI_RESET = "\u001B[0m"; //from      https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_CYAN = "\u001B[36m";
     static String user_name = "";
+    static int [][] grid = new int [4][4];
     public static void main(String[] args) throws IOException {
-        int[][] grid = new int [4][4];
         User_Access();
         String filename = "C:\\Users\\Leonard\\IdeaProjects\\2048\\src\\com\\company\\users.txt";
         ArrayList<String> users = Read_user(filename);
@@ -23,6 +28,8 @@ public class _2048 {
             }
             break;
         }
+        grid = check_user_file_exist();
+        System.out.println(Arrays.deepToString(grid));
     }
     public static void User_Access() {
         Scanner sc = new Scanner(System.in);
@@ -62,7 +69,7 @@ public class _2048 {
        output.close();
     }
 
-    public static void check_user_file_exist() throws IOException {
+    public static int[][] check_user_file_exist() throws IOException {
         Scanner sc = new Scanner(System.in);
         FileReader uf = new FileReader("C:\\Users\\Leonard\\IdeaProjects\\2048\\src\\com\\company\\grid.txt");
         BufferedReader in = new BufferedReader(uf);
@@ -71,34 +78,80 @@ public class _2048 {
         ArrayList<String> userGrid;
         userGrid = new ArrayList<>();
         while(line != null && i<i+1 && i<25) {
-            if (userGrid.get(i).contains(user_name)) {
+            if (line.equals(user_name)) {
                 System.out.println("Previous saved grid found. \n Would you like to contiune from previous saved grid? " +
                         "\n Yes to contiune, No if you want to start again");
                 boolean flag_cont = false;
                 while (!flag_cont) {
                     try {
                         String start_from_saved = sc.next();
-                        if (!start_from_saved.equals("Yes")||!start_from_saved.equals("No")){
-                            Exception UserinputIsInvalid = new Exception();
+                        if (!start_from_saved.equals("Yes")&&!start_from_saved.equals("No")){
+                           Exception UserinputIsInvalid = new Exception();
+                            throw UserinputIsInvalid;
                         }
-                        flag_cont = true;
-                        System.out.println("Loading saved game...");
+                        else if(start_from_saved.equals("No")){
+                            flag_cont = true;
+                            break;
+                        }
+                        else{
+                            flag_cont = true;
+                            read_user_grid(i,grid);
+                            break;
+                        }
 
                     } catch (InputMismatchException e) {
                         System.out.println("Please input a string");
                     }
-                    catch (Exception UserinputException){
+                    catch ( Exception UserinputIsInvalid){
                         System.out.println("Please input 'Yes' to load from saved game, 'No' to start from a new grid");
                     }
                 }
-                i++;
+                break;
             }
+            i++;
         }
+     return grid;
     }
+    public static int[][] read_user_grid (int i, int[][] grid) throws IOException {
+        FileReader uf = new FileReader("C:\\Users\\Leonard\\IdeaProjects\\2048\\src\\com\\company\\grid.txt");
+        BufferedReader in = new BufferedReader(uf);
+        String usergrid = " ";
+        System.out.println("Loading saved game...");
+        for(int j=0; j<i+2; j++) {
+            usergrid = in.readLine();
+        }
+        System.out.println(usergrid);
+        String[] usrgrid = usergrid.split(" ");
+System.out.println(Arrays.toString(usrgrid));
+        int count = 0;
+            for(int l=0; l<4; l++){
+
+                for(int m =0; m<4;m++) {
+                   usrgrid[count] = usrgrid[count].trim();
+                    grid[l][m] = Integer.parseInt((usrgrid[count]));
+                    count++;
+                }
+                count++;
+            }
+        return grid;
+    }
+   // public static int[][] Generate_Random_Grid(int [][] grid){
+
+  //  }
+   // public static int[][] rotate(int[][]grid){
+
+ //   }
+  //  public static int[][] move(int[][]grid,int direction){
+
+    //}
+   // public static int[][] Random_Tile(){
+
+   // }
+
     public static void write_highest_score(){
 
     }
-    public static void save_highest_score(){
+   public static void load_leaderboard(){
 
-    }
+   }
 }
